7ac1d69a0f217982a0f447d82de574f7b514e0de
VAD <36716074+viccy2@users.noreply.github.com>
2025-01-02 12:11:05 +0100

    access the element in array

--- Data_structures/Array/array.py
+++ Data_structures/Array/array.py
@@ -15,12 +15,24 @@ for i in range(len(array)):
 a = 10
 b = 2
 array.insert(b, a)
-print(array)    
+print(array)
 
-# array removal
+# array deletion
 c = 6
 if c in array:
     array.remove(c)
 else:
     print(c, 'not in array')
 print(array)
+
+
+# array searching
+def find_element(element):
+    for i in range(len(array)):
+        if array[i] == element:
+            print('found')
+        else:
+            print("element not found")
+
+
+find_element(1)
